e2aa3bebe9b10eaf2d4062c4e757dddb6b159ebf
John Cashmore <j.cashmore@building-blocks.com>
2013-02-13 12:53:22 +0000

    IE Font Fix
    Fix for webfonts WSOD issue, only works if yous stylesheets have .css
    extension

--- selectivizr.js
+++ selectivizr.js
@@ -440,7 +440,7 @@ References:
 	// and recursivly replaces @import rules with their contents, ultimately
 	// returning the full cssText.
 	function parseStyleSheet( url ) {
-		if (url) {
+		if (url && url.indexOf(".css") !== -1) {
 			return loadStyleSheet(url).replace(RE_COMMENT, EMPTY_STRING).
 			replace(RE_IMPORT, function( match, quoteChar, importUrl, quoteChar2, importUrl2, media ) {
 				var cssText = parseStyleSheet(resolveUrl(importUrl || importUrl2, url));
