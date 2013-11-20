7da4348059166f2a0b93b3a380d9a40419958bb0
KeesCBakker <kees.c.bakker@gmail.com>
2013-11-20 15:33:17 +0100

    Fix bug in IE8 when loading empty stylesheets
    Loading an empty stylesheet might result in the `xhr.responseText` being `null`. Rest of the code expect an EMPTY_STRING. Returning null will cause a JavaScript error.

--- selectivizr.js
+++ selectivizr.js
@@ -394,7 +394,7 @@ References:
 	function loadStyleSheet( url ) {
 		xhr.open("GET", url, false);
 		xhr.send();
-		return (xhr.status==200) ? xhr.responseText : EMPTY_STRING;	
+		return (xhr.status==200) ? (xhr.responseText != null ? xhr.responseText : EMPTY_STRING) : EMPTY_STRING;	
 	};
 	
 	// --[ resolveUrl() ]---------------------------------------------------
