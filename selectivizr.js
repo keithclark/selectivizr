673c4e3d34e6c0abd6dd974c8ce99e931d7ec162
Ridge <ridge.liu@toursforfun.com>
2014-08-26 13:33:10 +0800

    EMPTY_STRING

--- selectivizr.js
+++ selectivizr.js
@@ -436,7 +436,7 @@ References:
 		try {
 			xhr.open("GET", url, false);
 		} catch (ex) {
-			return RE_ORIGIN.test(url) ? loadStyleSheet(url.replace(RE_ORIGIN, "")) : EMPTY_STRING;
+			return RE_ORIGIN.test(url) ? loadStyleSheet(url.replace(RE_ORIGIN, EMPTY_STRING)) : EMPTY_STRING;
 		}
 		xhr.send();
 		if(xhr.status === 200){
